f3d7aa6934e4f1bc5e4f32f666094ce7bbc76ef8
WST <ilya.averkov@gmail.com>
2020-06-27 21:49:57 +0300

    Убрал пару рудиментов

--- src/headers/maycloud/config.h
+++ src/headers/maycloud/config.h
@@ -1,18 +1,5 @@
 #ifndef MAYCLOUD_CONFIG_H
 #define MAYCLOUD_CONFIG_H
-
-/**
- * На данном этапе есть смысл обрабатывать выключение/выключение перекодирования аудио дефайнами
- * Возможно, в дальнейшем это не понадобится
- */
-//#define CAMCAP_REMUX_AUDIO_ON
-#define CAMCAP_RECODE_AUDIO_ON
-
-/**
-* Поддержка GNU TLS
-*/
-#undef HAVE_GNUTLS
-
 /**
 * Размер блока файлового буфера
 */
@@ -27,25 +14,5 @@
 * Размер буфера чтения
 */
 #define FD_READ_CHUNK_SIZE 4096
-
-/**
-* Поддержка zlib сконфигурирована?
-*/
-#undef HAVE_LIBZ
-
-/**
-* Уроверь компресси в zlib
-*/
-#define ZLIB_COMPRESS_LEVEL 6
-
-/**
-* Размер блока-буфера компрессии zlib
-*/
-#define ZLIB_DEFLATE_CHUNK_SIZE 4096
-
-/**
-* Размер блока-буфера для декомпрессии zlib
-*/
-#define ZLIB_INFLATE_CHUNK_SIZE (FD_READ_CHUNK_SIZE * 8)
 
 #endif // MAYCLOUD_CONFIG_H

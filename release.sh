ee85915d160ba45ff2fc515790e9ec26ab80a005
WST <ilya.averkov@gmail.com>
2020-07-07 20:36:47 +0300

    Показывать сгенерированный deb-пакет, для удобства

--- release.sh
+++ release.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+border () {
+	local str="$*"      # Put all arguments into single string
+	local len=${#str}
+	local i
+	for (( i = 0; i < len - 12; ++i )); do
+		printf '='
+	done
+	printf "\n| $str |\n"
+	for (( i = 0; i < len - 12; ++i )); do
+		printf '='
+	done
+	echo
+}
+
 make distclean
 cmake -DCMAKE_BUILD_TYPE=Release .
 make -j4
 cpack -G DEB
+
+echo
+border "Created: `ls --color *.deb` — `ls -lha *.deb | awk -F " " {'printf $5'}`"
+echo

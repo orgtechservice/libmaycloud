180237ff5a591aeef18c1a804cf110547f03ec18
WST <ilya.averkov@gmail.com>
2020-07-30 12:51:14 +0300

    Пара правок обёртки к шаблонизатору

--- src/headers/maycloud/htmltemplate.h
+++ src/headers/maycloud/htmltemplate.h
@@ -1,15 +1,25 @@
 #ifndef HTML_TEMPLATE_H
 #define HTML_TEMPLATE_H
 
+#include <maycloud/json.hpp>
 #include <maycloud/inja.hpp>
 
 class HtmlTemplate
 {
+protected:
+	std::string _code;
+	nlohmann::json _context;
+
 public:
 	HtmlTemplate(const HtmlTemplate&) = delete;
 	HtmlTemplate(const std::string &filename);
 	HtmlTemplate();
 	~HtmlTemplate();
+
+	void setCode(const std::string &code);
+	std::string toString();
+	std::string render();
+	void set(const std::string &tag, const std::string &value);
 };
 
 #endif
--- src/htmltemplate.cpp
+++ src/htmltemplate.cpp
@@ -6,9 +6,25 @@ HtmlTemplate::HtmlTemplate(const std::string &filename) {
 }
 
 HtmlTemplate::HtmlTemplate() {
-
+	_code = "";
 }
 
 HtmlTemplate::~HtmlTemplate() {
 
+}
+
+void HtmlTemplate::setCode(const std::string &code) {
+	_code = code;
+}
+
+std::string HtmlTemplate::toString() {
+	return inja::render(_code, _context);
+}
+
+std::string HtmlTemplate::render() {
+	return toString();
+}
+
+void HtmlTemplate::set(const std::string &tag, const std::string &value) {
+	_context[tag] = value;
 }
